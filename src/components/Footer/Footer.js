4965c7e2e113216500013f249c8e3705a1b2d969
duyanh3110 <duyanh19962012@gmail.com>
2021-05-11 04:28:19 +0300

    Create Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="copyright">
+        © 2020 Fasterpay Ltd. All rights reserved. Fasterpay Limited (FRN:
+        900681) is authorised by the Financial Conduct Authority under the
+        Electronic Money Regulations 2011 for the issuing of electronic money
+        and payment instruments.
+      </div>
+      <div className="secure">
+        <img
+          alt="secure"
+          src={process.env.PUBLIC_URL + "assets/secure-logo.svg"}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
